be675091c77ccca6dfd051a2312d3f5b4c6392b1
Adrian Kaufmann <addi.kaufmann@bluewin.ch>
2020-02-11 22:33:07 +0100

    added test

--- CarRentApi/CarRentApi.test/CarClassTest.cs
+++ CarRentApi/CarRentApi.test/CarClassTest.cs
@@ -2,6 +2,8 @@ using System.Linq;
 using CarRentApi.Controllers.BaseData;
 using CarRentApi.Model;
 using CarRentApi.Repositories.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 
@@ -66,6 +68,32 @@ namespace CarRentApi.test
                 }
             }
             Assert.IsTrue(bOk);
+        }
+
+        [Test]
+        public void Post_CarClassExisting_IsTrue()
+        {
+            var carclass = new CarClass() { Class = "Ichwerdegeloescht", CostsPerDay = 1000m };
+            var carclass2 = new CarClass() { Class = "Ichwerdegeloescht", CostsPerDay = 1000m };
+
+            DbContextOptionsBuilder<CarRentDBContext> builder = new DbContextOptionsBuilder<CarRentDBContext>();
+            builder.UseInMemoryDatabase("CarRent");
+            DbContextOptions<CarRentDBContext> options = builder.Options;
+            CarRentDBContext carrent = new CarRentDBContext(options);
+            ExampleData.ExampleData.InitTestData(carrent);
+            var carclasscontroller = new CarClassesController(carrent);
+            carclasscontroller.PostCarClass(carclass);
+            var x = carclasscontroller.PostCarClass(carclass2);
+
+            int Count = 0;
+            foreach (var carsclasses in carclasscontroller.GetCarClasses())
+            {
+                if (carsclasses.Class.Equals(carclass.Class) && carsclasses.CostsPerDay == carclass.CostsPerDay)
+                {
+                    Count++;
+                }
+            }
+            Assert.IsTrue(Count == 1);
         }
 
     }
